756ac302df3b21289e92a225d1cfe2fcf597435c
Alexandre Gallucci <alexandre.gallucci@scrt.ch>
2016-08-16 11:47:25 +0200

    Lot of upgrades.

--- metavas.py
+++ metavas.py
@@ -1,20 +1,58 @@
 #!/usr/bin/python3
+
+"""Metavas.
+
+Usage:
+    metavas.py
+    metavas.py -u <user> -w <password> -c <config> -n <hostname> -i <interface>
+
+    Options:
+        --version     Show version.
+
+"""
+
 import getpass
 import subprocess
 from subprocess import check_output
 from xml.etree import ElementTree
-import sys
 import progressbar
 import time
 import requests
 import sys
+from docopt import docopt
+
+def arg_check_int(name, value):
+    if value is None:
+        return -1
+    else:
+        try:
+            return int(value)
+        except Exception as e:
+            print("Bad " + name + " index.")
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+
+    manual_mode = False
+    arguments = docopt(__doc__, version='MetaVAS 1.0alpha')
+    user = "" if arguments['<user>'] is None else arguments['<user>']
+    if (not user): manual_mode = True
+    passwd = "" if arguments['<password>'] is None else arguments['<password>']
+    hostname = "" if arguments['<hostname>'] is None else arguments['<hostname>']
+    interface = "" if arguments['<interface>'] is None else arguments['<interface>']
+    #target_choice = arg_check_int("target", arguments['<target>'])
+    target_choice = -1
+    config_choice = arg_check_int("config", arguments['<config>'])
+
+    
 
 configs = []
-target_upload = "localhost/upload"
+target_upload     = "localhost/upload"
 create_target_xml = "<create_target><name>{}</name><hosts>{}</hosts></create_target>"
-create_task_xml = "<create_task><name>{}</name><preferences><preference>\
-                   <scanner_name>{}</scanner_name><value>{}</value>\
-                   </preference></preferences><config id=\"{}\"/><target id=\"{}\"/></create_task>"
+create_task_xml   = "<create_task><name>{}</name><preferences><preference>\
+                     <scanner_name>{}</scanner_name><value>{}</value>\
+                     </preference></preferences><config id=\"{}\"/><target id=\"{}\"/></create_task>"
 
 
 def send_file(target, file):
@@ -28,8 +66,8 @@ def send_file(target, file):
         r = requests.post(protocol + target, files=files, verify=VERIFY)
         print("File sent : " + r.text)
     except IOError as e :
+        print ("[-] Could not upload file")
         print ("[-] IOError : {1}".format(e.errno, e.strerror))
-
 
 def is_int(s):
     try: 
@@ -38,21 +76,20 @@ def is_int(s):
     except ValueError:
         return False
 
-
 print ("\n  [ LOGIN ]")
 # Get config list
 logged = False
 while (logged == False):
-    user = input("\n[?] OpenVAS username : ")
-    passwd = getpass.getpass("[?] OpenVAS password : ")
+    if (not user): user = input("\n[?] OpenVAS username : ")
+    if (not passwd): passwd = getpass.getpass("[?] OpenVAS password : ")
     try:
         lines = check_output(["omp", "-u", user, '-w', passwd, '-T']).decode("utf-8").splitlines()
         logged = True
-    except Exception as e:
-        pass
-
+    except subprocess.CalledProcessError as e:
+        print ("[-] Could not connect to OpenVAS.\n[!] Please check that services openvas-manager and openvas-scanner are started.")
+        sys.exit(1)
+        
 print ("\n[+] Succesfully logged in.")
-
 
 # Target
 
@@ -60,34 +97,48 @@ targets = []
 print ("\n  [ TARGET ]")
 try:
     for line in lines:
-        print (line)
         split = str(line).split("  ", 1)
         targets.append((split[0], split[1]))
 except Exception as e:
     print ("[-] " + e.message)
     pass
 
-
-
 target_is_string = False
-target_choice = -1
-print ("\n  -- Target list --\n")
+if (manual_mode): print ("\n  -- Target list --\n")
 for i, target in enumerate(targets):
-    print ("  [" + str(i) + "] " + target[1])
+    if (manual_mode): print ("  [" + str(i) + "] " + target[1])
 
-while (target_choice < 0 or target_choice >= len(targets) or target_is_string):
+while (not hostname and (target_choice < 0 or target_choice >= len(targets) or target_is_string)):
     try:
         target_choice = input("\n[>] Select target index OR press [N]ew  : ")
         target_choice = int(target_choice)
     except Exception as e:
-        if (target_choice.upper() == "N"):
+        if (type(target_choice) is str and target_choice.upper() == "N"):
             target_is_string = True
             break
         else:
             target_choice = -1
 
-if (not target_is_string):
+
+if (1 == 2):
     target_id = targets[target_choice][0]
+    status = -1
+    while (status != "201"):
+        try:
+            #target_name = input("\n[>] Enter target label : ")
+            target_name = "Target_"  + time.strftime("%Y-%m-%d-%H-%M%-S") 
+            if not hostname: hostname = input("[>] Enter hostname to scan : ")
+            lines = subprocess.check_output(["omp", "-u", user, '-w', passwd, '--xml', create_target_xml.format(target_name, hostname)]).decode("utf-8").splitlines()
+            root = ElementTree.fromstring(lines[0])
+            status = root.get('status')
+            if (status != "201"):
+                print ("\n[!]" + root.get('status_text'))
+        except Exception as e:
+            print ("[-] " + e.message)
+            pass
+    target_id = root.get('id')
+    print ("\n[+] Succesfully created TARGET with ID : " + target_id)
+
 else:
     print ("\n  [ CONFIG ]")
 
@@ -97,8 +148,6 @@ else:
         configs.append((split[0], split[1]))
 
 
-    # Config
-    config_choice = -1
     while (config_choice < 0 or config_choice >= len(configs) ):
         print ("\n  -- Configuration list --\n")
         for i,cfg in enumerate(configs):
@@ -112,25 +161,9 @@ else:
     config_id = configs[config_choice][0]
     config_text = configs[config_choice][1]
 
-    status = -1
-    while (status != "201"):
-        try:
-            #target_name = input("\n[>] Enter target label : ")
-            target_name = "Target_"  + time.strftime("-%Y%m%d-%H%M%S") 
-            host = input("[>] Enter hostname to scan : ")
-            lines = subprocess.check_output(["omp", "-u", user, '-w', passwd, '--xml', create_target_xml.format(target_name, host)]).decode("utf-8").splitlines()
-            root = ElementTree.fromstring(lines[0])
-            status = root.get('status')
-            if (status != "201"):
-                print ("\n[!]" + root.get('status_text'))
-        except Exception as e:
-            print ("[-] " + e.message)
-            pass
-    target_id = root.get('id')
-    print ("[+] Succesfully created TARGET with ID : " + target_id)
+    
 
-
-print ("\n  [ TASK ]\n")
+print ("\n  [ TASK ]")
 status = -1
 
 #Create task
@@ -138,10 +171,10 @@ while (status != "201"):
     try:
         #task_name = input("\n[>] Enter task label : ")
         task_name = "Task_"  + time.strftime("-%Y%m%d-%H%M%S") 
-        scanner_name = input("[>] Enter scanner label : ")
-        value = input("[>] Enter \'" + scanner_name + "\' value : ")
+        scanner_name = "source_iface"
+        if (not interface): interface = input("[>] Enter source interface value : ")
         lines = subprocess.check_output(["omp", "-u", user, '-w', passwd, '--xml',\
-            create_task_xml.format(task_name, scanner_name, value, config_id, target_id)]).decode("utf-8").splitlines()
+            create_task_xml.format(task_name, scanner_name, interface, config_id, target_id)]).decode("utf-8").splitlines()
         root = ElementTree.fromstring(lines[0])
         status = root.get('status')
         if (status != "201"):
@@ -156,14 +189,15 @@ print ("\n[+] Succesfully created TASK with ID : " + task_id)
 
 print ("\n  [ SCAN ]")
 
-go = input("\n[>] Start the scan NOW ? (Y/n)")
+
+go = input("\n[>] Start the scan NOW ? (Y/n)") if (manual_mode) == "True" else "Y"
 if (go == 'n' or go == 'N'):
     print ("[!] Exiting...")
     sys.exit(0)
 else:
     lines = subprocess.check_output(["omp", "-u", user, '-w', passwd, '-S', task_id]).decode("utf-8").splitlines()
     report_id = lines[0]
-    print ("[+] Scan launched, Report ID : " + report_id)
+    print ("\n[+] Scan launched, Report ID : " + report_id + "\n")
 
 percent = 0;
 status != "Running"
@@ -185,18 +219,16 @@ while (True):
       bar.finish()
       break
 
-print ("Report completed.")
+print ("\n[+] Report ready.\n")
 #//start scan ?
 
 subprocess.check_output(["omp", "-u", user, '-w', passwd, '-R', report_id, '>', 'report_id' + '.report']).decode("utf-8").splitlines()
 
-
 report = subprocess.check_output(["omp", "-u", user, '-w', passwd, '-R', report_id]).decode("utf-8")
-
 
 filename = report_id + ".xml"
 report_file = open(report_id + ".xml", "w")
-text_file.write(report)
-text_file.close()
+report_file.write(report)
+report_file.close()
 
 send_file(target_upload, filename)
